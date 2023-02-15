507986f317336d2cd734aae3ffa16f5294445e20
Paloma Sales <81493259+palomasales@users.noreply.github.com>
2023-02-15 12:59:24 -0300

    Implementando a AnimalDAO e tela de cadastro do animal

--- src/java/br/com/petShopPrado/dao/AnimalDAO.java
+++ src/java/br/com/petShopPrado/dao/AnimalDAO.java
@@ -1,5 +1,34 @@
 package br.com.petShopPrado.dao;
 
+import br.com.petShopPrado.model.Animal;
+import br.com.petShopPrado.connection.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class AnimalDAO {
-    
+
+    Connection con;
+
+    public AnimalDAO() {
+        con = ConnectionFactory.getConnection();
+    }
+
+    public void cadastrarAnimal(Animal animal) {
+        PreparedStatement stmt = null;
+        String sql = "INSERT INTO animal(especie, nome) VALUE(?, ?)";
+        try {
+            stmt = con.prepareStatement(sql);
+            stmt.setString(1, animal.getEspecie());
+            stmt.setString(2, animal.getNome());
+            stmt.execute();
+            stmt.close();
+
+        }catch(SQLException u){
+            throw new RuntimeException(u);
+        
+        }
+
+    }
+
 }
--- src/java/br/com/petShopPrado/dao/TutorDAO.java
+++ src/java/br/com/petShopPrado/dao/TutorDAO.java
@@ -17,7 +17,7 @@ public class TutorDAO {
 
     public void cadastrar(Tutor tutor) {
         PreparedStatement stmt = null;
-        String sql = "INSERT INTO client(nome, cpf, idade, email, telefone, endereco, cidade, cep, token, id_client) VALUES( ?,  ?,  ?,  ?, ?, ?, ?, ?, ?, 1)"; 
+        String sql = "INSERT INTO client(nome, cpf, idade, email, telefone, endereco, cidade, cep, token) VALUES( ?,  ?,  ?,  ?, ?, ?, ?, ?, ?)"; 
         try {
             stmt = con.prepareStatement(sql);
             stmt.setString(1, tutor.getNome());
